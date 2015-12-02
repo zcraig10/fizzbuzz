5b3ef021e9ce85a75422b2aedf950bd0a83fc977
Zack Craig <nagada10@aol.com>
2015-12-02 14:50:44 -0500

    refactor

--- js/app.js
+++ js/app.js
@@ -1,15 +1,46 @@
-prompt("Please enter a number:")
+// Fizzbuzz Refactor
+
+var number = parseInt(prompt("Please enter a number:"));
+  while (isNaN(number)) {
+    number = parseInt(prompt("Please try again:"));
+  } 
+
+function fizzbuzz(num) {
+
+  for (i = 1; i <= num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("Fizzbuzz!");
+    }
+    else if (i % 3 == 0) {
+      console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+        console.log(i);
+      }
+  }
+}
+
+fizzbuzz(number);
+
+// Multiple Fizzbuzz Printers
+
+
+
+/* Original Fizzbuzz
 
 for (i = 1; i <= 15; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("Fizzbuzz!");
+     console.log("Fizzbuzz!");
+   }
+   else if (i % 3 == 0) {
+     console.log("Fizz");
+   }
+   else if (i % 5 == 0) {
+     console.log("Buzz");
+   } else {
+       console.log(i);
+      }
   }
-  else if (i % 3 == 0) {
-    console.log("Fizz");
-  }
-  else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-      console.log(i);
-    }
-}
+*/
